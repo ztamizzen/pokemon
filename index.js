5230cae3eef04038a8a0dff1de64292aac98e069
Mats Lindblad <mats.lindblad@isotop.se>
2017-06-27 15:26:36 +0200

    initial commit, following Dave Ceddias tut

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/passwords', (req, res) => {
+    const count = 5;
+    const passwords = Array.from(Array(count).keys()).map(i => generatePassword(12, false));
+    res.json(passwords);
+    console.log(`Sent ${count} passwords`);
+});
+
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`Password generator listening on port: ${port}`);
